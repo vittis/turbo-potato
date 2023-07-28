848ae441399ce6c736ae39023ef8c10b0eb1f984
vittis <vitbolb1@gmail.com>
2023-07-28 04:00:34 -0300

    add more mechanics

--- apps/potato-worker/modules/game/game/Game.ts
+++ apps/potato-worker/modules/game/game/Game.ts
@@ -98,11 +98,11 @@ export class Game {
         this.boardManager,
         OWNER.TEAM_TWO,
         POSITION.BOT_MID,
-        Races.Elf,
+        Races.Dwarf,
         Classes.Knight,
         {
-          mainHandWeapon: Weapons.Wand as WeaponData,
-          chest: Chests.ClothRobe as ArmorData,
+          mainHandWeapon: Weapons.Greatsword as WeaponData,
+          chest: Chests.LeatherShirt as ArmorData,
           head: Heads.ClothHat as ArmorData,
         }
       )
@@ -277,53 +277,7 @@ DANO TOTAL:
 
 /* 
 
-    MOD PENETRATION
-    T1: + 5 
-    T2: + 10
-    T3: + 15
-    T4: + 20
-
-    MOD DEX
-    T1: + 2
-    T2: + 4
-    T3: + 7
-    T4: + 10
-
-    MOD Damage
-    T1: + 10
-    T2: + 15
-    T3: + 25
-    T4: + 35
-
-
-    Longsword TBlueprint
-    Sem Mods
-
-    Longsword T1:
-    DexT1
-
-    Longsword T2:
-    DamageT2 + DexT1
-
-    Longsword T3:
-    DamageT3 + PenT1 + DexT1
-
-    Longsword T4:
-    DamageT4 + Pen2 + DexT1
-
-    Longsword T5:
-    DamageT5 + PenT3 + DexT1 ou DamageT5 + PenT4 
-
-
-    Item sempre vai ter um MOD do próprio tier
-    Os outros mods vão ser randomizados e somados tem valor de TierDoItem - 1
-    Cada Mod tem seu peso de raridade
-
-    
-    Weapon blueprint: One handed sword, One handed mace
-    Weapon Example: Katana, Gladius, Great axe
-
-    Na blueprint é definido os possíveis modifiers q podem ser rollados no item.
+   
 
 
     Batalha baseada em buffs/debuffs tipo super auto battlemon
@@ -331,102 +285,18 @@ DANO TOTAL:
 
 ___________________________________________________________________________________________________
 
-
-{
-    "actorId": "14",
-    "type": "ATTACK",
-    "payload": {
-      "targetsId": ["00"],
-      "stats": {
-        "ap": 7.719999999997981,
-        "sp": 712
-      },
-      "modifiers": {
-        "sp": 56
-      }
-    },
-    "step": 99,
-    "subEvents": [{
-      "actorId": "00",
-      "type": "RECEIVED_DAMAGE",
-      "payload": {
-        "stats": {
-          "hp": 84,
-          "armorHp": 0,
-          "sp": 517.05
-        },
-        "modifiers": {
-          "hp": -23,
-          "sp": 195.75
-        }
-      }
-   }]
-},
-
-{
-  "actorId": "00",
-  "type": "CAST_SKILL",
-  "payload": {
-    "targetsId": ["00"],
-    "skillName": "Healing Word"
-  },
-  "step": 188,
-  "subEvents": [{
-    "actorId": "00",
-    "type": "RECEIVED_HEAL",
-    "payload": {
-      "stats": {
-        "hp": 71,
-      },
-      "modifiers": {
-        "hp": 24,
-      }
-    }
-  }]
-}
-
-{
-  "actorId": "14",
-  "type": "CAST_SKILL",
-  "payload": {
-    "targetsId": ["00"],
-    "skillName": "Head Crush"
-  },
-  "step": 188,
-  "subEvents": [{
-    "actorId": "00",
-    "type": "RECEIVED_DAMAGE",
-    "payload": {
-      "stats": {
-        "hp": 84,
-        "armorHp": 0,
-        "sp": 517.05
-      },
-      "modifiers": {
-        "hp": -23,
-        "sp": 195.75
-      }
-    }
- },
- {
-  "actorId": "00",
-  "type": "RECEIVED_DISABLE",
-  "payload": {
-    "disableName": "Stun",
-    "duration": 20,
-  }
-}]
-}
+dex = each point increase X% attack speed (X is a constant, like 5 (5%)) 
+OR start with X fast
 
 
-/* 
 
 STATUS EFFECTS
 
 REGEN = heal 1 hp per stack per 5 steps and remove 1 stack
-BUFF ATTACK SPEED = increase 1 attack speed per stack and remove 1 per attack
-BUFF ATTACK DAMAGE = increase 1 attack damage per stack and remove 1 per attack
-BUFF SKILL REGEN = increase 1 skill regen per stack and remove 1 per ult cast
+BUFF ATTACK SPEED (FAST) = increase 1 attack speed per stack and remove 1 per attack
+BUFF SKILL SPEED (FOCUS) = increase 1 skill speed per stack and remove 1 per skill used
+BUFF ATTACK DAMAGE (A. DAMAGE) = increase 1 attack damage per stack and remove 1 per attack
+BUFF SKILL DAMAGE (S. DAMAGE) = increase 1 skill damage per stack and remove 1 per skill use
 POISON = deal 1 dmg per stack per 5 steps and remove 1 stack
 BLEED = deal 1 dmg per stack per 5 steps and remove 1 stack
 SLOW = reduce 1 attack speed per stack per 5 steps and remove 1 stack per attack
@@ -434,6 +304,11 @@ VULNERABLE = increase 1% damage taken per stack, remove all on hit
 STURDY = reduce 1% damage taken per stack, remove 1 stack on hit
 THORN = deal 1 dmg per stack on hit and remove 1 stack
 TAUNT = force enemy units to attack this unit, remove 1 stack on hit
+MULTISTRIKE = on weapon attack/skill immediately trigger the attack again more X times and remove all stacks
+
+
+
+
 
 
 DISABLE
@@ -488,7 +363,7 @@ PERKS CATEGORIES
 EX: APLICA POISON ON HIT
 
 - STATS BUFF
-EX: +20 BUFF ATTACK SPEED
+EX: BATTLE START: GAIN 20 FAST
 
 - INSTANT EFFECT
 EX: HEAL X HP ON TRIGGER
@@ -500,3 +375,218 @@ PARKS HAVE TIERS (1, 2, 3, 4, 5) THAT CAN BE STACKED THROUGH DIFERRENT SOURCES
 EX: GAIN AGILE PERK LVL 1 FROM CLASS, AND LVL 2 FROM DAGGER, THEN UNIT HAS AGILE PERK LVL 3
 
  */
+
+/* 
+
+Classes brainstorm
+
+
+
+------------------------------------------------------------------
+Blacksmith
+- Gain skill: 
+  - Reinforce Allies: Give adjacent allies 20 SHIELD / CD: X
+
+- Skill Trees (or subclasses) 
+  - Weaponsmith
+    - Tier 1:
+      - ???: "Reinforce Allies" also gives 5 FAST to adjacents allies
+      - ???: "Reinforce Allies" also gives 10 DAMAGE and 10 FAST to FRONT ally
+    - Tier 2 (req 2):
+      - ???: "Reinforce Allies" also gives 1 MULTISTRIKE to FRONT ally
+  - Armorsmith
+    - Tier 1:
+      - ???: "Reinforce Allies" also gives gives 15 more SHIELD to adjacents allies
+      - ???: "Reinforce Allies" give 10 STURDY and 20 more SHIELD to FRONT ally
+    - Tier 2 (req 2):
+      - ???: "Reinforce Allies": Give 2 TAUNT and 25 THORN to FRONT ally
+
+    - Utility skills 
+      - "Reinforce Allies" don't affect adjacents units anymore. Apply the same amount to self
+      - BATTLE START: Trigger base skill 
+      - DEATH: Trigger base skill 
+------------------------------------------------------------------
+
+RANGER
+  - Gain attack: 
+    - Weak Spot: Attack furthest enemy and apply 10 Slow / CD: 7s
+  - Skill Trees
+    - Sniper
+      - Tier 1: 
+        - Gain 1 Ranged Proficiency
+        - Gain 1 Open Field Tactics
+      - Tier 2 (req 2): 
+        - Gain attack: POWERSHOT: Attack all enemies on a row with 70% attack damage. Requires physical ranged weapon
+    - Hunter
+      - Tier 1: 
+        - Gain skill: Summon Crab: Give adjacent allies 5 STURDY and 5 THORN / CD: 8
+        - Gain skill: Summon Rabbit: Give adjacent allies 10 FAST / CD: X
+        - Gain skill: Summon Boar: Attack X damage and apply 10 STUN on hit / CD: X
+      - Tier 2 (req 1):
+        - All team summon skills 15% less base cooldown
+      - Tier 3 (req 3):
+        - DEATH: Trigger all summons skill
+
+  - Utility tree
+    - Desperate Will: ALLY DEATH: Gain 5 A. DAMAGE
+    - Surprise Assault: BATTLE START: Give 5 SLOW to all enemies
+    - Nature Insight: BATTLE START: Gain 15 FOCUS
+
+---------------------------------------------------------------------------
+
+
+
+BOW MODS POOL: 
+  - Gain X Ranged Profiency (Specialized)
+  - Gain X Field Tactics ()
+  - Gain X Coat In Poison (Serpent)
+  - Gain X Heavy Puncher
+  - Gain X Swift Spellcasting
+  - Gain attack: Weak Spot: Attack furthest enemy and apply 10 Slow / CD: 7s
+
+
+---------------------------------------------------------------------------
+PERKS: 
+  - Ranged Proficiency: BATTLE START: Gain X FAST if using ranged physical weapons
+  - Open Field Tactics: BATTLE START: Gain X FAST if no ally in front
+  - Coat In Poison: On Attack Hit: Apply X Poison
+  - Penetrating Attack: ON WEAPON HIT: DEALS X% damage to enemy behind the target
+  - Disarming Blow: ON ATTACK HIT: Apply X VULNERABLE
+  - Heavy Puncher: BATTLE START: GAIN 10 A. DAMAGE
+  - Arcane Potency: BATTLE START: GAIN 10 S. DAMAGE
+  - Swift Spellcasting: BATTLE START: GAIN 10 FOCUS
+
+---------------------------------------------------------------------------
+POSSIBLE BOWS:
+  - T0
+    - Longbow
+      - Gain attack: Deals BASE_DAMAGE / CD: X 
+      - Gain 1 Ranged Proficiency (implicit)
+    - Shortbow (T0)
+      - Gain attack: Deals BASE_DAMAGE / CD: X
+      - Gain 1 Disarming Blow (implicit)
+    - Viscous Bow
+      - Gain attack: Deals BASE_DAMAGE / CD: X
+      - Gain 1 Coat In Poison (implicit)
+
+BOWS T1:
+  - Specialized Longbow (T1)
+    - Gain attack: Deals BASE_DAMAGE / CD: X (implicit)
+    - Gain 1 Ranged Proficiency (implicit)
+    - Gain 1 Ranged Proficiency (MOD T1)
+  - Arcane Shortbow (T1)
+    - Gain attack: Deals BASE_DAMAGE  / CD: X (implicit)
+    - Gain 1 Disarming Blow (implicit)
+    - Gain 1 Swift Spellcasting (MOD T1)
+
+BOWS T2:
+ - Tactical Longbow 
+    - Gain attack: Deals BASE_DAMAGE / CD: X (implicit)
+    - Gain 1 Ranged Proficiency (implicit)
+    - Gain 2 Open Field Tactics (MOD T2)
+    - Gain 1 Heavy Puncher (MOD T1)
+
+BOWS T3:
+  - Fatal Viscous Bow 
+    - Gain attack: Deals BASE_DAMAGE / CD: X (implicit)
+    - Gain 1 Coat In Poison (implicit)
+    - Gain 3 Disarming Blow (MOD T3)
+    - Gain 2 Coat In Poison (MOD T2)
+  - Impaler Viscous Bow 
+    - Gain attack: Deals BASE_DAMAGE / CD: X (implicit)
+    - Gain 1 Coat In Poison (implicit)
+    - Gain 3 Penetrating Attack (MOD T3)
+    - Gain 1 Coat In Poison (MOD T1)
+    - Gain 1 Heavy Puncher (MOD T1)
+
+
+ITEMS:
+    Gain X Ranged Proficiency
+
+
+    Longsword TBlueprint
+    Sem Mods
+
+    Longsword T1:
+    DexT1
+
+    Longsword T2:
+    DamageT2 + DexT1
+
+    Longsword T3:
+    DamageT3 + PenT1 + DexT1 ou DamageT3 + PenT2
+
+    Longsword T4:
+    DamageT4 + Pen2 + DexT1
+
+    Longsword T5:
+    DamageT5 + PenT3 + DexT1 ou DamageT5 + PenT4 
+
+
+    Item sempre vai ter um MOD do próprio tier
+    Os outros mods vão ser randomizados e somados tem valor de TierDoItem - 1
+    Cada Mod tem seu peso de raridade
+
+    
+    Weapon blueprint: One handed sword, One handed mace, bow
+    Weapon Example: Katana, Gladius, Great axe
+
+    Na blueprint é definido os possíveis modifiers q podem ser rollados no item.
+
+
+
+    Longbow T0 (base)
+    Sem Mods
+
+    Longbow T1:
+    DexT1
+
+    Longbow T2:
+    DamageT2 + DexT1
+
+    Longbow T3:
+    DamageT3 + PenT1 + DexT1
+
+    Longbow T4:
+    DamageT4 + Pen2 + DexT1
+
+    Longbow T5:
+    DamageT5 + PenT3 + DexT1 ou DamageT5 + PenT4 
+
+
+
+
+
+RAÇAS, bonuses que liberam qdo upa
+
+RACIAL BONUS LV 1: 
+RACIAL BONUS LV 3:  
+RACIAL BONUS LV 5: 
+
+
+
+
+
+
+
+
+
+- NEXT STEPS:
+
+VITU: 
+- obsidian bonito
+- remover mecanicas antigas
+- implementar novo sistema de ataques/skills
+- status effects
+
+GULM
+- Arrumar front (usar render texture)
+- Classes/Perks
+- ajudar vitu
+
+
+
+
+
+
+*/
